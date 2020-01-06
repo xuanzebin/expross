474f7e24636125ad6357539273d21934671e77fb
benectic <xuanzebin@126.com>
2020-01-06 15:36:01 +0800

    feat: 第四次迭代

--- test/index.js
+++ test/index.js
@@ -1,14 +1,46 @@
 const express = require('../lib/expross')
 const app = express()
+const router = express.Router()
 
-app.get('/', function(req, res, next) {
-  next('route');
-}).put('/', function(req, res, next) {
-  next(new Error('error fuck'));
-}).get('/', function(req, res) {
-  res.send('third');
+// app.get('/', function(req, res, next) {
+//   console.log('err')
+//   next('route')
+//   res.send('thi2rd');
+// }).get('/1', function(req, res, next) {
+//   next(new Error('error fuck'));
+// }).get('/', function(req, res) {
+//   res.send('third');
+// });
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
+
+// router.use(function (req, res, next) {
+//   console.log('Time:', Date.now());
+// });
+
+app.use(function(req, res, next) {
+  console.log('Time：', Date.now());
+  next();
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/', function(req, res, next) {
+  res.send('first');
+});
+
+
+router.use(function(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+router.use('/1', function(req, res, next) {
+  res.send('second');
+});
+
+app.use('/user', router);
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!');
+});
