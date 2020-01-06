3f16f32b523b6f465d37b2be4a2860032cbf4422
benectic <xuanzebin@126.com>
2020-01-06 21:31:35 +0800

    feat: 第六次迭代，增加模板渲染

--- test/index.js
+++ test/index.js
@@ -25,14 +25,14 @@ const router = express.Router()
 //   next();
 // });
 
-app.get('/', function(req, res, next) {
-  throw new Error('hi')
-  res.send('first');
-});
-app.use(function(err, req, res, next) {
-  console.log(err, '123')
-  res.send('error')
-})
+// app.get('/', function(req, res, next) {
+//   // throw new Error('hi')
+//   res.send('first');
+// });
+// app.use(function(err, req, res, next) {
+//   console.log(err, '123')
+//   res.send('error')
+// })
 
 
 // router.use(function(req, res, next) {
@@ -45,6 +45,25 @@ app.use(function(err, req, res, next) {
 // });
 
 // app.use('/user', router);
+
+var fs = require('fs'); // 此模板引擎依赖 fs 模块
+app.engine('ntl', function (filePath, options, callback) { // 定义模板引擎
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(new Error(err));
+    // 这是一个功能极其简单的模板引擎
+    var rendered = content.toString().replace('#title#', '<title>'+ options.title +'</title>')
+    .replace('#message#', '<h1>'+ options.message +'</h1>');
+    return callback(null, rendered);
+  });
+});
+
+app.set('views', './test/views'); // 指定视图所在的位置
+app.set('view engine', 'ntl'); // 注册模板引擎
+
+
+app.get('/', function(req, res, next) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
